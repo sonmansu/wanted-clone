98a442ee6f6b43398cbe7609c96b1ebcd55061b3
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-10-07 00:57:46 +0900

    design: 헤더 영역 개선
    * 헤더 영역 변경 사항
    - 헤더 내부에 left-btns-wrap, right-btns-wrap을 따로 만듦
      => 레이아웃 배치를 justify-content: space-between으로 주기 위함
    - 각주(footnote) 구현
    
    * main 영역 변경 사항
    - main의 article 태그들을 section 태그로 변경

---
+++ home.js
@@ -0,0 +1,52 @@
+
+let careerInsights = {
+    'imgs': [
+        'https://image.wanted.co.kr/optimize?src=http%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fservice%2Fuser%2F9kS%2Fimage%2FNYyzp7--e8G1MqXahXh-n8qXYhU.jpeg&w=500&q=75',
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fwanted.video.kr.kollus.com%2Fkr%2Fsnapshot%2Fwanted%2F20211108%2F5%2F52227108.jpg&w=500&q=75`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon152.png&w=140&q=75`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon152.png&w=140&q=75`,
+        `https://image.wanted.co.kr/optimize?src=http%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fservice%2Fuser%2FbxZK%2Fimage%2FD3ZvS-OetrtrFm4yJQXEwx6X6qA.jpg&w=500&q=75`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fwanted.video.kr.kollus.com%2Fkr%2Fsnapshot%2Fwanted%2F20201229%2F8%2F99233240.jpg&w=500&q=75`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fwanted.video.kr.kollus.com%2Fkr%2Fsnapshot%2Fwanted%2F20201229%2F9%2F96649545.jpg&w=500&q=75`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fwanted.video.kr.kollus.com%2Fkr%2Fsnapshot%2Fwanted%2F20201228%2F6%2F54160958.jpg&w=500&q=75`
+    ],
+    'titles': [
+        `트럼프도 따봉하는 '프로덕트 오너 마인드셋 Top 3'`,
+        `Live Talk : 직장 내 관계의 어려움 해결하기 - 박정환`,
+        `리더도 리더가 필요합니다.`,
+        `리더십은 만병통치약이 아니다`,
+        `디지털전환시대에 효과적인리더십`,
+        `조직 생존을 위한 Digital Leadership - 오용석`,
+        `복잡한 시대, 리더가 유지해야 할 것과 유발해야 할 것 - 정영재`,
+        `패널토론 New Leadership & HRD Re-Design - 조해리, 신제구, 임창현`
+    ],
+    'contents': [
+        `프로덕트 오너 Product Owner는 정신머리를 잘 붙들고 있어야합니다. 딸린 식구가 많기 때문입니다.`,
+        `사람은 사회적 동물이지만, 늘 인간관계에 대한 고민을 하면서 살아갑니다.인간관계가 힘들고 어`,
+        `리더의 성장과 성공을 도와주는 리더가 있어야 합니다. [1] 조직에서 임원이나 팀장님들을 만날 `,
+        `블라인드에 80명의 조직을 맡으며 번아웃 오고 있다는 한 스타트업 리더의 글에 지인들과 의견이 오`,
+        `국가과학기술연구회 주관으로 진행된 정부출연 25개 연구기관의 연구행정혁신추진단 전문분과`,
+        `최근 수많은 기업들은 생존하기 위해, 언텍트를 인정하고 빠르게 디지털화를 시도하고 있습니다. 이`,
+        `지금 조직들이 가장 많이 스터디하고 있는 'Agile'에 대한 이해를 통해 바람직한 리더의 역할`
+    ],
+    'icons': [
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon120.png&w=60&q=90`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Ffavicon%2F144x144.png&w=60&q=90`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon120.png&w=60&q=90`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon120.png&w=60&q=90`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon120.png&w=60&q=90`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Ffavicon%2F144x144.png&w=60&q=90`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Ffavicon%2F144x144.png&w=60&q=90`,
+
+    ],
+    'writers': [
+        `키릴 kiril`,
+        `박정환`,
+        `그로플 백종화`,
+        `ssoo`,
+        `변화디자이너 허일무박사`,
+        `SAP Korea 오용석`,
+        `LS그룹 정영재`,
+        `서울대학교 신제구, mySUNI임창현`
+    ]
+}

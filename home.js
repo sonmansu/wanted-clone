0e4e97cf7e99fe30ffe013ed135582b32d0f4778
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-10-07 19:09:31 +0900

    design: 메인 영역 개선
    * 변경사항
    - section 내부에 section-wrap 박스 생성
      -> section은 가로 너비를 꽉 채우고 section-wrap은 1060px 맞추기 위함.
     -> section을 1060px로 주면 안되는 이유는 스크롤 내려보면 전체 폭을 다 차지하는 section 영역이 하나 있기 때문
    - home.js에 section insight 콘텐츠 배열 생성
    - 글 추천 section 영역 작성

--- home.js
+++ home.js
@@ -20,14 +20,15 @@ let careerInsights = {
         `복잡한 시대, 리더가 유지해야 할 것과 유발해야 할 것 - 정영재`,
         `패널토론 New Leadership & HRD Re-Design - 조해리, 신제구, 임창현`
     ],
-    'contents': [
+    'descs': [
         `프로덕트 오너 Product Owner는 정신머리를 잘 붙들고 있어야합니다. 딸린 식구가 많기 때문입니다.`,
         `사람은 사회적 동물이지만, 늘 인간관계에 대한 고민을 하면서 살아갑니다.인간관계가 힘들고 어`,
         `리더의 성장과 성공을 도와주는 리더가 있어야 합니다. [1] 조직에서 임원이나 팀장님들을 만날 `,
         `블라인드에 80명의 조직을 맡으며 번아웃 오고 있다는 한 스타트업 리더의 글에 지인들과 의견이 오`,
         `국가과학기술연구회 주관으로 진행된 정부출연 25개 연구기관의 연구행정혁신추진단 전문분과`,
         `최근 수많은 기업들은 생존하기 위해, 언텍트를 인정하고 빠르게 디지털화를 시도하고 있습니다. 이`,
-        `지금 조직들이 가장 많이 스터디하고 있는 'Agile'에 대한 이해를 통해 바람직한 리더의 역할`
+        `지금 조직들이 가장 많이 스터디하고 있는 'Agile'에 대한 이해를 통해 바람직한 리더의 역할`,
+        `본 글은 개인 경험을 바탕으로 서술된 글입니다. 정답이 아닌 생각의 실마리를 제공하고 각자 나름의`
     ],
     'icons': [
         `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon120.png&w=60&q=90`,
@@ -35,6 +36,7 @@ let careerInsights = {
         `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon120.png&w=60&q=90`,
         `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon120.png&w=60&q=90`,
         `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fstatic%2Ficon%2Fios%2Ficon120.png&w=60&q=90`,
+        `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Ffavicon%2F144x144.png&w=60&q=90`,
         `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Ffavicon%2F144x144.png&w=60&q=90`,
         `https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Ffavicon%2F144x144.png&w=60&q=90`,
 
@@ -50,3 +52,53 @@ let careerInsights = {
         `서울대학교 신제구, mySUNI임창현`
     ]
 }
+
+let $sectionContentsUl = document.querySelector('.section-contents__ul');
+// console.log('$sectionContentsUl :>> ', $sectionContentsUl);
+
+function paintInsightContents() {
+    for (let i = 0; i < careerInsights.imgs.length; i++) {
+        const $li = `
+        <li class="section-contents__li">
+            <a href="#">
+                <div class="section-contents__thumb"><img src=${careerInsights.imgs[i]}" alt="콘텐츠 썸네일"></div>
+                <div class="section-contents__title">${careerInsights.titles[i]}</div>
+                <div class="section-contents__desc">${careerInsights.descs[i]}</div>
+                <div class="section-contents__writer">
+                    <button class="section-contents__writer__button"><img src=${careerInsights.icons[i]}" alt="유튜브 버튼"></button>
+                    <span class="section-contents__writer__title">${careerInsights.writers[i]}</span>
+                </div>
+            </a>
+        </li>
+        `
+        $sectionContentsUl.insertAdjacentHTML('beforeend', $li);
+    }
+
+    // careerInsights.forEach(item => {
+    //     console.log('item :>> ', item);
+        // const $li = document.createElement('li');
+        // const $a = document.createAttribute('a');
+
+        // const $divThumb = document.createElement('div');
+        // $divThumb.classList.add('section-contents__thumb');
+        // const $divThumbImg = document.createElement('img');
+        // $divThumbImg.src = item.imgs;
+        // $divThumb.appendChild($divThumbImg);
+
+        // const $divTitle = document.createElement('div');
+        // $divTitle.innerText = item.titles;
+
+        // const $divDesc = document.createElement('div');
+        // $divDesc.innerText = item.descs;
+
+        // const $divWriter = document.createElement('div');
+        // const $btnWriter = document.createElement('button');
+        // $btnWriter.classList.add('section-contents__writer__button')
+        // $divDesc.innerText = item.descs;
+
+
+
+    // });    
+}
+
+paintInsightContents();

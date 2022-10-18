c345830632e0444f2ddfac936d884dba84774890
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-10-18 12:24:09 +0900

    design: 메인, 채용 페이지 슬릭 슬라이더 영역 개선
    - variableWidth: true 값 줘서 !important 없이도 아이템 width가 1060px가 적용되도록 만듦
    - 자동 스크롤 시간 조정
    - 왼쪽, 오른쪽 화살표 버튼 위치 조정

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,7 @@
 $(function () {
+  /** 메인 슬라이더 **/
   $('.section-slider__list').slick({
     centerMode: true,
-    // centerPadding: '50px',
-    // centerPadding: '230px',
     centerPadding: 0,
     centeredSlides: true,
     variableWidth: true,
@@ -10,11 +9,11 @@ $(function () {
     infinite: true, 	//무한 반복 옵션	 
     slidesToShow: 1,		// 한 화면에 보여질 컨텐츠 개수
     slidesToScroll: 1,		//스크롤 한번에 움직일 컨텐츠 개수
-    speed: 100,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
+    speed: 500,	 // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
     arrows: true, 		// 옆으로 이동하는 화살표 표시 여부
     //   dots : true, 		// 스크롤바 아래 점으로 페이지네이션 여부
-    // autoplay: true,			// 자동 스크롤 사용 여부
-    //   autoplaySpeed : 10000, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
+    autoplay: true,			// 자동 스크롤 사용 여부
+    autoplaySpeed: 5000, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
     pauseOnHover: true,		// 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
     vertical: false,		// 세로 방향 슬라이드 옵션
     prevArrow: `<button class="section-slider__btn"><img src="https://img.icons8.com/external-outline-stroke-bomsymbols-/91/000000/external-arrow-digital-design-outline-set-2-outline-stroke-bomsymbols-.png" alt="right button"></button>`,		// 이전 화살표 모양 설정

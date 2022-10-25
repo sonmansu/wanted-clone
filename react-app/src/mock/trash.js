d6691f9e786716952a2b03c79d613154ca171071
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-10-25 22:52:23 +0900

    docs: 참고용 임시 파일 추가

---
+++ react-app/src/mock/trash.js
@@ -0,0 +1,5 @@
+// {
+//     "img": "https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fevents%2F2437%2F30d52ee0.jpg&w=800&q=75",
+//     "title": "",
+//     "tag": ""
+// },

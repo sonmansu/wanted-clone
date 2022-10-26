73882bb1927f1db9402c1cb4701c979fa4a138fd
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-10-26 14:37:35 +0900

    style: props로 column이 2개인지에 대한 여부를 전달받아 썸네일 이미지에 variant-css 적용

--- react-app/src/component/ContentThumbnail.jsx
+++ react-app/src/component/ContentThumbnail.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/contentThumbnail.css';
 
-const ContentThumbnail = ({ src }) => {
+const ContentThumbnail = ({ src, is2Col }) => {
   return (
-    <div class="section-contents__thumb">
+    <div
+      className={
+        is2Col
+          ? 'section-contents__thumb section-contents__thumb--big'
+          : 'section-contents__thumb'
+      }
+    >
       <img src={src} alt="콘텐츠 썸네일" />
     </div>
   );

a9bbdd93ebfe42f4f017f143866058bb894404c0
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-10-31 18:30:01 +0900

    refactor: silderItem.css 내부 클래스명 단순하게 변경
    - info 내부 divider에 부모 컴포넌트의 css가 우선순위가 더 높게 적용되고 있어서 부모에서 사용하던 css 를 적용하지 않고 silderItem.css에 작성

--- react-app/src/components/SliderItem.jsx
+++ react-app/src/components/SliderItem.jsx
@@ -3,16 +3,16 @@ import '../styles/sliderItem.css';
 
 const SliderItem = ({ img, title, desc }) => {
   return (
-    <div className="section-slider__item">
-      <img className="section-slider__img" src={img} alt="" />
-      <div className="section-slider__info">
-        <h2 className="section-slider__info__title">{title}</h2>
-        <h3 className="section-slider__info__desc">{desc}</h3>
-        <div className="divider divider--info"></div>
-        <button className="section-slider__info__go-btn">
-          <span className="section-slider__info__go-btn__text">바로가기</span>
+    <div className="item-wrapper">
+      <img className="item__img" src={img} alt="" />
+      <div className="item-info-wrapper">
+        <h2 className="item-info__title">{title}</h2>
+        <h3 className="item-info__desc">{desc}</h3>
+        <div className="item-info__divider"></div>
+        <button className="item-info__go-btn">
+          <span className="item-info__go-btn__text">바로가기</span>
           <img
-            className="section-slider__info__go-btn__img rotate-180"
+            className="item-info__go-btn__img rotate-180"
             src="https://img.icons8.com/external-outline-stroke-bomsymbols-/91/000000/external-arrow-digital-design-outline-set-2-outline-stroke-bomsymbols-.png"
             alt="right button"
           />

6587cf92352f3168c6071a09778e12c80724f730
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-03 14:11:28 +0900

    feat: 검색된 포지션이 0개일 때 보여줄 div 생성

--- react-app/src/pages/SearchPage.jsx
+++ react-app/src/pages/SearchPage.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import '../styles/searchPage.css';
 import { useSearchParams } from 'react-router-dom';
+import PositionFilter from '../components/PositionFilter';
+import PositionList from '../components/PositionList';
+import { useState } from 'react';
 
 const SearchPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const searchKeyword = searchParams.get('query');
+  const [positionCnt, setPositionCnt] = useState(0);
   return (
     <div>
       <div className="search-keyword-box">
         <button className="search-keyword-text">{searchKeyword}</button>
       </div>
-      <div className="searched-box">
-        <div className="position-row">
-          <h2 className="position-text">
-            포지션<span>0</span>
-          </h2>
+      <div className="searched-background">
+        <div className="searched-wrapper">
+          <div className="position-row">
+            <h2 className="position-text">
+              포지션<span>{positionCnt}</span>
+            </h2>
+          </div>
+          <PositionFilter />
+          <PositionList
+            searchKeyword={searchKeyword}
+            setPositionCnt={setPositionCnt}
+          />
+          {positionCnt === 0 && (
+            <div className="searched-position-none-wrapper">
+              <p>바로 지원할 곳이 없다면?</p>
+              <br />
+              <p>매치업에 등록하고 면접제안 받으세요!</p>
+              <button className="register-btn">매치업에 등록하기</button>
+            </div>
+          )}
         </div>
       </div>
     </div>

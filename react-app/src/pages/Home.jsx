09744a6e242793b4ba777c020d338ea6824b2c90
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-05 00:31:03 +0900

    feat: 홈페이지에 구독 신청 영역, 채용 정보 영역 추가
    home.css에 관련 css 파일 추가

--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -7,7 +7,6 @@ import SlideBanner from '../components/SlideBanner';
 import '../styles/home.css';
 import ListContainer from '../components/ListContainer2';
 import QuestionMarkIcon from 'assets/icons/QuestionMarkIcon';
-import BtnCircle from 'components/BtnCircle';
 import insightArr from 'mock/insights.json';
 import { Link } from 'react-router-dom';
 import InsightCategoriesSlider from 'components/InsightCategoriesSlider';
@@ -63,16 +62,84 @@ const Home = () => {
         <ListContainer listType="article" />
       </Section>
       <div className="divider"></div>
-      <LineBanner kind="recommend" />
       <Section title="직장인을 위한 Wanted+ VOD" subtitle="VOD 전체보기">
         <VodList />
       </Section>
+      <LineBanner kind="recommend" />
       <Section
         title="커리어 성장을 위한 맞춤 이벤트"
         subtitle="이벤트 전체보기"
       >
         <EventList />
       </Section>
+      <section class="section-subscribe">
+        <div class="section-wrap section-subscribe-wrap">
+          <div class="section-subscribe-left">
+            <p class="section-subscribe__text-big">
+              <span class="bold">wanted +</span>구독해야 하는 이유
+            </p>
+            <p class="section-subscribe__text-small">
+              구독자의 서류합격률이 비구독자보다 1.5배 높아요!
+            </p>
+            <div class="section-subscribe__btn">
+              <i class="xi-check"></i>
+              <span>첫 구독 0원 신청하기</span>
+            </div>
+          </div>
+          <div class="section-subscribe__bg-img"></div>
+        </div>
+      </section>
+      <section>
+        <div class="section-wrap">
+          <h2 class="section-title">채용 정보를 찾고 계셨나요?</h2>
+          <ul class="section-recruit-ul">
+            <li class="section-recruit-ul__li">
+              <Link>
+                <div class="section-recruit-ul__top">
+                  <Icon size={32} icon="magnifyingGlass" />
+                </div>
+                <div class="section-recruit-ul__bottom">
+                  <span class="section-recruit-ul__text">채용공고</span>
+                  <Icon size={16} icon="arrowRight" />
+                </div>
+              </Link>
+            </li>
+            <li class="section-recruit-ul__li">
+              <Link>
+                <div class="section-recruit-ul__top">
+                  <Icon size={32} icon="user" />
+                </div>
+                <div class="section-recruit-ul__bottom">
+                  <span class="section-recruit-ul__text">내 프로필</span>
+                  <Icon size={16} icon="arrowRight" />
+                </div>
+              </Link>
+            </li>
+            <li class="section-recruit-ul__li">
+              <Link>
+                <div class="section-recruit-ul__top">
+                  <Icon size={32} icon="building" />
+                </div>
+                <div class="section-recruit-ul__bottom">
+                  <span class="section-recruit-ul__text">매치업</span>
+                  <Icon size={16} icon="arrowRight" />
+                </div>
+              </Link>
+            </li>
+            <li class="section-recruit-ul__li">
+              <Link>
+                <div class="section-recruit-ul__top">
+                  <Icon size={32} icon="filter" />
+                </div>
+                <div class="section-recruit-ul__bottom">
+                  <span class="section-recruit-ul__text">직군별 연봉</span>
+                  <Icon size={16} icon="arrowRight" />
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 };

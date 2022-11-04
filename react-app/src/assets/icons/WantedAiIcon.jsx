50151d6562d7b4810bce242259610bf11604b46e
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-04 21:37:06 +0900

    add: src/assets/icons에 icon 파일들 추가

---
+++ react-app/src/assets/icons/WantedAiIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function WantedAiIcon({ sizeClass }) {
+  return (
+    <img
+      className={sizeClass}
+      src="https://static.wanted.co.kr/images/ai/logo-wantedai.png"
+      alt="wanted ai 로고"
+    />
+  );
+}

060501f5868a66ea8aa655c1d90c50bd1a6d524a
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-09 11:44:58 +0900

    feat: 비밀번호 모달창에서 옳은 비밀번호인지 확인하는 기능, 로그인 기능
    - 맞는 비밀번호면 setLogin을 true로 설정하고 localStorage에 저장시킴 (이는 로그인 상태가 유지되게 하기 위함)

--- react-app/src/components/LoginModal/PasswordModal.jsx
+++ react-app/src/components/LoginModal/PasswordModal.jsx
@@ -1,12 +1,31 @@
 import BlueRadiusBtn from 'components/BlueRadiusBtn';
 import React from 'react';
+import userArr from '../../mock/users.json';
 import { useState } from 'react';
 
-export default function PasswordModal({ setActivatedModal }) {
+export default function PasswordModal({ setActivatedModal, setLogin }) {
   const [pw, setPw] = useState('');
+  const [isCorrectPw, setCorrectPw] = useState(false);
+  const [isClickedNextBtn, setIsClickedNextBtn] = useState(false);
 
   const onChangePw = (e) => {
     setPw(e.target.value);
+    setIsClickedNextBtn(false);
+  };
+
+  const onClikNextBtn = () => {
+    const inputEmail = sessionStorage.getItem('inputEmail');
+    const user = userArr.find((user) => user.email === inputEmail);
+    if (user.pw === pw) {
+      setActivatedModal(0);
+      localStorage.setItem('email', inputEmail);
+      localStorage.setItem('pw', pw);
+      sessionStorage.removeItem('inputEmail');
+      setLogin(true);
+    } else {
+      setCorrectPw(false);
+    }
+    setIsClickedNextBtn(true);
   };
 
   return (
@@ -28,7 +47,11 @@ export default function PasswordModal({ setActivatedModal }) {
           <label className="input__label">비밀번호</label>
           <div className="input-wrap">
             <input
-              className="input"
+              className={
+                pw.length > 0 && !isCorrectPw && isClickedNextBtn
+                  ? 'input input--border-red'
+                  : 'input'
+              }
               type="password"
               //   value={pw}
               placeholder="비밀번호를 입력해주세요."
@@ -36,10 +59,16 @@ export default function PasswordModal({ setActivatedModal }) {
               onChange={onChangePw}
             />
           </div>
+          {pw.length > 0 && !isCorrectPw && isClickedNextBtn ? (
+            <div className="input__errorMessage">
+              비밀번호가 일치하지 않습니다.
+            </div>
+          ) : null}
         </div>
         <BlueRadiusBtn
           text="다음"
           className={pw.length > 0 ? '' : 'unactivated'}
+          onClick={onClikNextBtn}
         />
         <button className="pw-change-btn">
           <span className="pw-change-btn__text">비밀번호 초기화/변경</span>

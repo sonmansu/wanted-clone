ac49fae52c320f6271b2dc2b6e5c895cb27aa55c
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-10 17:24:52 +0900

    refactor: UserDropdown 각 아이템 클릭 가능 영역을 부모 너비로 넓힘

--- react-app/src/components/Header/UserDropdown.jsx
+++ react-app/src/components/Header/UserDropdown.jsx
@@ -13,34 +13,39 @@ export default function UserDropdown({ setLogin }) {
     <div className="user-dropdown-wrap">
       <div className="user-dropdown">
         <ul className="user-dropdown__list">
-          <li className="user-dropdown__item">
-            <Link>MY 원티드</Link>
+          <li>
+            <Link className="user-dropdown__item">MY 원티드</Link>
           </li>
-          <li className="user-dropdown__item">
-            <Link>프로필</Link>
-          </li>
-          <div className="user-dropdown__divider"></div>
-          <li className="user-dropdown__item">
-            <Link>지원 현황</Link>
-          </li>
-          <li className="user-dropdown__item">
-            <Link>제안받기 현황</Link>
-          </li>
-          <li className="user-dropdown__item">
-            <Link>좋아요</Link>
-          </li>
-          <li className="user-dropdown__item">
-            <Link>북마크</Link>
+          <li>
+            <Link className="user-dropdown__item">프로필</Link>
           </li>
           <div className="user-dropdown__divider"></div>
-          <li className="user-dropdown__item">
-            <Link>추천</Link>
+          <li>
+            <Link className="user-dropdown__item">지원 현황</Link>
           </li>
-          <li className="user-dropdown__item">
-            <Link>포인트</Link>
+          <li>
+            <Link className="user-dropdown__item">제안받기 현황</Link>
           </li>
-          <li className="user-dropdown__item user-dropdown__logout-item">
-            <button onClick={onClickLogout}>로그아웃</button>
+          <li>
+            <Link className="user-dropdown__item">좋아요</Link>
+          </li>
+          <li>
+            <Link className="user-dropdown__item">북마크</Link>
+          </li>
+          <div className="user-dropdown__divider"></div>
+          <li>
+            <Link className="user-dropdown__item">추천</Link>
+          </li>
+          <li>
+            <Link className="user-dropdown__item">포인트</Link>
+          </li>
+          <li>
+            <button
+              className="user-dropdown__item user-dropdown__logout-item"
+              onClick={onClickLogout}
+            >
+              로그아웃
+            </button>
           </li>
         </ul>
       </div>

d02d2b5c164c5882e1b51d1ed099f8758295209d
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-17 16:01:20 +0900

    refactor: SearchTagItem.jsx 컴포넌트 제거, TagBtn 스타일드 컴포넌트로 대체
    - SearchTagItem.jsx, searchTagItem.css 파일 제거
    - PositionFilter.jsx에 TagBtn 스타일드 컴포넌트 작성 후 사용 중

--- react-app/src/components/PositionFilter.jsx
+++ react-app/src/components/PositionFilter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/positionFilter.css';
-import SearchTagItem from '../components/SearchTagItem';
 import searchTagsText from '../mock/searchTags.json';
+import styled from 'styled-components';
 
 const PositionFilter = () => {
   return (
@@ -32,16 +32,41 @@ const PositionFilter = () => {
       <div className="divider divider--margin"></div>
       <div className="search-tag-slider-box">
         {searchTagsText.map((tag, idx) => (
-          <SearchTagItem
-            key={idx}
-            text={tag.text}
-            img={tag.img}
-            bgCol={tag.bgCol}
-          />
+          <TagBtn key={idx} bgCol={tag.bgCol}>
+            <span>{tag.text}</span>
+            <img src={tag.img} alt={tag.text} />
+          </TagBtn>
         ))}
       </div>
     </div>
   );
 };
+
+const TagBtn = styled.button`
+  background-color: ${(props) => props.bgCol};
+  height: 32px;
+  border-radius: 32px;
+  padding: 8px 14px;
+  /* padding: 8px 25px 8px 14px; ? img 크기까지 포함해서 오른쪽 패딩 조정 */
+  box-sizing: border-box;
+  margin-right: 8px;
+  color: #333333;
+  font-size: 13px;
+  display: flex;
+  align-items: center;
+  white-space: nowrap; /* 이거 안주면 텍스트에 자동개행 생김 */
+  /* word-break: nowrap; */
+  cursor: pointer;
+  border: 1px solid transparent;
+
+  &:hover {
+    border: 1px solid rgba(51, 102, 255, 0.3);
+  }
+  img {
+    width: 16px;
+    height: 16px;
+    margin-left: 5px;
+  }
+`;
 
 export default PositionFilter;

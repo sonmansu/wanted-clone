38fce87aa6744647f57c2b725e5c8177dda1d761
SONSOOMIN <80534651+sonmansu@users.noreply.github.com>
2022-11-19 02:36:02 +0900

    refactor: EventKindItem.jsx 컴포넌트를 EventKind 스타일 컴포넌트로 변경
    - EventItem.jsx 외에서는 사용안되는 컴포넌트라 같은 파일 내에 작성함
    - 기존 EventKindItem.jsx, eventKindItem.css 파일 제거

--- react-app/src/components/EventItem.jsx
+++ react-app/src/components/EventItem.jsx
@@ -1,24 +1,66 @@
 import React, { useState } from 'react';
 import ContentThumbnail from './ContentThumbnail';
 import ContentTitle from './ContentTitle';
-import EventKindItem from './EventKindItem';
 import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 import 'styles/eventItem.css';
 
 const EventItem = ({ img, kinds, title }) => {
-  // console.log(kinds);
-  const eventKinds = kinds.map((kind) => <EventKindItem kind={kind} />);
   return (
     <li>
       <Link>
         <ContentThumbnail big>
           <img src={img} alt="콘텐츠 썸네일" />
         </ContentThumbnail>
-        <div className="event-kinds-box">{eventKinds}</div>
+        <div className="event-kinds-box">
+          {kinds.map((kind) => (
+            <EventKind>
+              {kind === '온라인' && <i className="xi-central-signal" />}
+              {kind}
+            </EventKind>
+          ))}
+        </div>
         <ContentTitle>{title}</ContentTitle>
       </Link>
     </li>
   );
 };
+
+const EventKind = styled.span`
+  height: 20px;
+  display: inline-flex;
+  align-items: center;
+  font-size: 11px;
+  font-weight: 700;
+  padding: 0 6px;
+  border: 1px solid black;
+  border-radius: 2px;
+  background-color: white;
+  margin-right: 5px;
+
+  i {
+    margin-right: 4px;
+  }
+
+  ${(props) => {
+    // console.log(props.children);
+    if (props.children[1] === '온라인') {
+      return css`
+        background-color: black;
+        color: white;
+      `;
+    } else if (props.children[1] === '네트워킹') {
+      return css`
+        border-color: #a149e4;
+        color: #a149e4;
+      `;
+    } else if (props.children[1] === '아티클') {
+      return css`
+        border-color: #ff9100;
+        color: #ff9100;
+      `;
+    }
+  }}
+`;
 
 export default EventItem;
